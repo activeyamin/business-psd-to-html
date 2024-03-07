e0f2e4880059d590c5a9851825da310da8a18d5b
activeyamin <frontenddeveloperyamin@gmail.com>
2024-03-07 16:44:22 +0600

    first commit

---
+++ seopage1-job-task/js/script.js
@@ -0,0 +1,39 @@
+
+// ========HERO SLICK SLIDER START========
+$('.hero_slider_box').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  arrows: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+// ========HERO SLICK SLIDER END========
